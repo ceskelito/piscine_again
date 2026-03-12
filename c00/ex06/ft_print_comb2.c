ebd77d74f46e98e950d89b4ba5b1f22a5c55d038
ceske-42 <ceske@etik.com>
2026-03-12 13:42:57 +0100

    print_comb_2

--- c00/ex06/ft_print_comb2.c
+++ c00/ex06/ft_print_comb2.c
@@ -6,7 +6,7 @@
 /*   By: rceschel <rceschel@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/12 10:17:40 by rceschel          #+#    #+#             */
-/*   Updated: 2026/03/12 13:29:31 by rceschel         ###   ########.fr       */
+/*   Updated: 2026/03/12 13:39:43 by rceschel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 void	ft_putchar_quad(char a, char b, char c, char d)
 {
-
 	if (a == c && b == d)
 		return ;
 	write(1, &a, 1);
@@ -25,6 +24,25 @@ void	ft_putchar_quad(char a, char b, char c, char d)
 	if (a == '9' && b == '8' && c == '9' && d == '9')
 		return ;
 	write(1, ", ", 2);
+}
+
+void	inner_cicle(int a, int b)
+{
+	int	c;
+	int	d;
+
+	c = '0';
+	while (c <= '9')
+	{
+		d = '0';
+		while (d <= '9')
+		{
+			ft_putchar_quad(a, b, c, d);
+			d++;
+		}
+		c++;
+	}
+
 }
 
 void	ft_print_comb(void)
@@ -40,17 +58,7 @@ void	ft_print_comb(void)
 		b = '0';
 		while (b <= '8')
 		{
-			c = '0';
-			while (c <= '9')
-			{
-				d = '0';
-				while (d <= '9')
-				{
-					ft_putchar_quad(a, b, c, d);
-					d++;
-				}
-				c++;
-			}
+			inner_cicle(a, b);
 			b++;
 		}
 		a++;
@@ -58,7 +66,7 @@ void	ft_print_comb(void)
 	write(1, "\n", 1);
 }
 
-// int	main( void )
-// {
-// 	ft_print_comb();
-// }
+int	main( void )
+{
+	ft_print_comb();
+}
