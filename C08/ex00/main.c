06f3f47ca53b9f24ff75a98dc2c0ac9a6b44ab6b
ceske-42 <ceske@etik.com>
2026-03-25 20:35:45 +0100

    Placed in norme

--- C08/ex00/main.c
+++ C08/ex00/main.c
@@ -6,27 +6,27 @@
 /*   By: rceschel <rceschel@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/25 20:17:58 by rceschel          #+#    #+#             */
-/*   Updated: 2026/03/25 20:29:32 by rceschel         ###   ########.fr       */
+/*   Updated: 2026/03/25 20:34:49 by rceschel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
-#include <unistd.h>
-#include <stddef.h>
 #include <stdbool.h>
+#include <stddef.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-#define FILENAME_MISSING "File name missing."
-#define CANNOT_READ "Cannot read file."
-#define TOO_MANY_ARG "Too many arguments."
+#define FILENAME_MISSING "File name missing.\n"
+#define CANNOT_READ "Cannot read file.\n"
+#define TOO_MANY_ARG "Too many arguments.\n"
 
-int ft_strlen(const char *s)
+int	ft_strlen(const char *s)
 {
-	int i;
+	int	i;
 
 	i = 0;
-	while(s[i])
+	while (s[i])
 		i++;
 	return (i);
 }
@@ -35,46 +35,42 @@ int	throw_argc_error(int ac)
 {
 	if (ac < 2)
 	{
-		write(STDERR_FILENO, FILENAME_MISSING,
-			ft_strlen(FILENAME_MISSING));
+		write(STDERR_FILENO, FILENAME_MISSING, ft_strlen(FILENAME_MISSING));
 		return (1);
 	}
 	else if (ac > 2)
 	{
-		write(STDERR_FILENO, TOO_MANY_ARG,
-			ft_strlen(TOO_MANY_ARG));
+		write(STDERR_FILENO, TOO_MANY_ARG, ft_strlen(TOO_MANY_ARG));
 		return (1);
 	}
 	return (0);
-
 }
 
-int throw_cannot_read_error(int fd)
+int	throw_cannot_read_error(int fd)
 {
 	if (fd == -1)
 	{
-		write(STDERR_FILENO, CANNOT_READ,
-			ft_strlen(CANNOT_READ));
+		write(STDERR_FILENO, CANNOT_READ, ft_strlen(CANNOT_READ));
 		return (1);
 	}
 	return (0);
 }
 
-enum { BUFF_SIZE = 42 };
+#define BUFF_SIZE 42
 
-int main(int ac, char *av[])
+int	main(int ac, char *av[])
 {
-	int		fd;
-	int		buff[BUFF_SIZE];
-	int		bytes_read;
+	int	fd;
+	int	buff[BUFF_SIZE];
+	int	bytes_read;
 
-	if(throw_argc_error(ac))
+	if (throw_argc_error(ac))
 		return (0);
 	fd = open(av[1], O_RDONLY);
 	bytes_read = read(fd, buff, BUFF_SIZE);
 	if (throw_cannot_read_error(bytes_read))
 		return (0);
-	while(bytes_read > 0 && buff[0])
+	while (bytes_read > 0 && buff[0])
 	{
 		write(STDOUT_FILENO, buff, BUFF_SIZE);
 		bytes_read = read(fd, buff, BUFF_SIZE);
